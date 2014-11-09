52e157bcb96363a2b90c2becae5f31c52ac50267
Giovani Melo Marzano <giovani.marzano@gmail.com>
2014-11-09 17:22:32 -0200

    faceCsv: Script para processar csv bruto extraído do NodeXL
    O NodeXL, logo após a coleta do facebook, possui uma planílha que traz uma
    aresta para cada interação entre duas pessoas. Ou seja, se a pessoa A gostou
    2 vezes de algo que a pessoa B fez, então existirão 2 arestas do tipo Liker
    entre A e B.
    
    Este script agrega arestas do mesmo tipo criando uma outra coluna com a
    contagem de arestas agregadas.

---
+++ src/faceCsv.py
@@ -0,0 +1,62 @@
+"""Script que processa um arquivo csv que contém as arestas como extraídas do
+facebook pelo NodeXL.
+
+O csv de entrada tem o seguinte formato::
+
+    pessoa1;pessoa2;relação
+
+, onde relação é o tipo de interação do pessoa1 para o pessoa2.
+
+No arquivo aparecem multiplos registros com o mesmo valor de pessoa1, pessoa2
+e relação, que indicam ocorrências múltiplas daquela interação. As arestas são
+consideradas direcionadas, a menos que a relação apareça na lista
+RELACOES_NAO_DIRECIONADAS.
+
+O script gera outro csv com linhas na forma::
+    
+    id1;id2;relação;peso
+
+, onde peso é a contagem de interações do tipo 'relação' entre pessoa1 e
+pessoa2.
+"""
+
+import csv
+import sys
+
+RELACOES_NAO_DIRECIONADAS = ['Friend']
+
+RELACOES_INCLUIDAS = [
+    'Liker',
+    'Post Author',
+    'Commenter',
+    'User Tagged'
+]
+
+CSV_OPTIONS = {
+    'delimiter': ';'
+}
+
+if __name__ == '__main__':
+
+    pesos = {}
+
+    def addEdge(src, tgt, rel):
+        edge = (src, tgt, rel)
+        p = pesos.get(edge, 0)
+        p += 1
+        pesos[edge] = p
+
+    csvReader = csv.reader(open(sys.argv[1], 'r', newline=''), **CSV_OPTIONS)
+
+    for campos in csvReader:
+        if campos[2] in RELACOES_INCLUIDAS:
+            addEdge(campos[0], campos[1], campos[2])
+            if campos[2] in RELACOES_NAO_DIRECIONADAS:
+                addEdge(campos[1], campos[0], campos[2])
+                
+    with open(sys.argv[2], 'w', newline='') as f:
+        csvWriter = csv.writer(f, **CSV_OPTIONS)
+        for edge, p in pesos.items():
+            row = list(edge)
+            row.append(p)
+            csvWriter.writerow(row)
